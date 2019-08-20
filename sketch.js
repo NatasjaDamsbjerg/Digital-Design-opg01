09b6ba4fd9559babb88587ddfaf13422550083bd
NatasjaDamsbjerg <natasja@damsbjerg.dk>
2019-08-20 12:07:47 +0200

    GameOver mode lavet

--- sketch.js
+++ sketch.js
@@ -1,45 +1,66 @@
-/*
-Først laver vi et nogle variable til at lave en appelsin
- - en kugle som vi vil skyde afsted og fange i en turban
-*/ 
 
 var turban;
 var score = 0;
 var missed = 0;
 var appelsiner = []
+var header;
+//var status;
+var spil = true;
+var button;
 
-//Tid står for tid (Of course) hvilke er en mekanisme der bestemmer respawntiden
-
-/* 
- * 
- */
 function setup() {
-    var header = createElement('h1', 'Catch das Appelsinos');
+    /*Fra index.html hives der fat i id'et 'status' hvor jeg laver en tekst under mit canvas, 
+    der skal fortæller status på spillet.
+    */
+    var status = document.getElementById('status');
+    status.innerHTML = 'Igang';
+    header = createElement('h1', 'Catch das Appelsinos');
     createCanvas(750, 600);
+    status.parentNode.appendChild(status);
     x = rad;
     turban = new Kurv(670, 100, 70, 50, 10);
     appelsiner.push(new Appelsin(670, 100, 70, 50, 10));
 
+    /*Her sættes intervallet for hvornår der skal skydes en ekstra appelsin afsted, sådan at 
+    der er mere en 1 appelsin på skærmen af gangen.
+    */
     setInterval(function () {
         appelsiner.push(new Appelsin(670, 100, 70, 50, 10));
     }, 30000)
 }
 
 function draw() {
-    background(0);
-    for (var i = 0; i < appelsiner.length; i++) {
-        var appelsin = appelsiner[i]
-        appelsin.checkScore(turban);
-        appelsin.move();
-        appelsin.appelsin();
+    if (spil) {
+
+        background(0);
+        for (var i = 0; i < appelsiner.length; i++) {
+            var appelsin = appelsiner[i]
+            appelsin.checkScore(turban);
+            appelsin.move();
+            appelsin.appelsin();
+        }
+
+        display();
+
+        turban.move();
+    }else{
+        //Her vises Game Over skærmen
+        document.getElementById('status').innerHTML = 'Game Over';
+        button = createButton('Restart Game');
+        button.position(300, 375);
+        button.mousePressed(restartGame);
     }
+    if (missed < 1){
+        spil = false;
+    }
+}
 
-    display();
-
-    turban.move();
+function restartGame() {
+    location.reload();
 }
 
 function display() {
+    //Her opsættes tekstfelterne for score og liv, ved at kalde på variablerne der er lavet globalt.
     fill(255)
     text("Score: " + score, width - 80, 30);
     text("Life: " + missed, width - 80, 50);
@@ -57,55 +78,3 @@ function keyPressed() {
 function mousePressed() {
 
 }
-
-/*
-OPGAVER
- Opgave 1 - undersøg hvad variablerne  grav  og  tid  bruges til.
-            Skriv det i kommentarer, prøv at se hvad der sker, når
-            I laver dem om. 
-
- Opgave 2 - lav programmet om så det er tilfældigt hvor højt oppe 
-            på venstre kan appelsinerne starter. Overvej om man kan 
-            sikre, at appelsinen ikke ryger ud af skærmens top men 
-            stadig har en "pæn" bane
-
- Opgave 3 - lav programmet om så man også kan bevæge turbanen mod
-            højre og venstre med A- og D-tasterne. Prøv jer frem med
-            forskellige løsninger for hvor hurtigt turbanen skal rykke
-
- Opgave 4 - ret programmet til, så det også angives hvor mange 
-            appelsiner man IKKE greb med turbanen
-
- Opgave 5 - Undersøg hvad scriptet  kurv.js  er og gør, samt hvad de 
-            funktioner, scriptet indeholder, skal bruges til. Skriv 
-            det som kommentarer oven over hver funktion. Forklar tillige,
-            hvad sammenhængen mellem dette script og turbanen i hoved-
-            programmet er, og forklar det med kommentarer i toppen af 
-            kurv.js
-
- Opgave 6 - find et billede af en turban og sæt det ind i stedet 
-            for firkanten. Find eventuelt også en lyd, der kan afspilles, 
-            når appelsinen gribes. Se gerne i "p5 Reference" hvordan, 
-            hvis ikke I kan huske det:   https://p5js.org/reference/
-            Lav programmet om, så man kan flytte turbanen med musen
-
- Opgave 7 - lav en Appelsin-klasse, lige som der er en Kurv-klasse. 
-            Flyt koden til appelsinen ud i et selvstændigt script.
-            Overvej hvad det skal hedde, oghvilke variabler og funktioner, 
-            der skal lægges over i det nye script, herunder hvordan det 
-            kommer til at berøre turbanen. Skriv jeres overvejelser i 
-            kommentarerne
-
- Opgave 8 - ret programmet til, så der kan være flere appelsiner i 
-            luften på en gang, dvs. at der kan skydes en ny appelsin
-            afsted før den foregående er forsvundet. Overvej hvordan 
-            og hvor hurtigt de skal skydes af, og forklar jeres tanker
-            i kommentarerne
-
- Opgave 9 - ret programmet til, så det kan vindes og/eller tabes ved
-            at man griber eller misser et antal appelsiner. Sørg for 
-            at der vises en "Game Over"-skærm, som fortæller om man 
-            vandt eller tabte, og som giver mulighed for at starte et
-            nyt spil.
-
-*/
